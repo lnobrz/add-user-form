d0f2fd98c9f592901083bf278283379740f472c6
Fernandes <jose.fernandes@hp.com>
2022-10-19 23:38:25 -0300

    replace useState to useRef to store form's inputs values

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,32 +1,30 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FormContainer, Label, Input, Button } from "./styles";
 import WarningModal from "../warningModal";
 
 const Form = (props) => {
-  const [userName, setUserName] = useState("");
-  const [userAge, setUserAge] = useState("");
+  const userNameRef = useRef();
+  const userAgeRef = useRef();
   const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const handleNameInput = (event) => {
-    setUserName(event.target.value);
-  };
-
-  const handleAgeInput = (event) => {
-    setUserAge(event.target.value);
-  };
 
   const handleSubmit = () => {
     props.usersSetter(() => [
       ...props.usersArray,
-      { name: userName, age: userAge, id: Math.random() },
+      {
+        name: userNameRef.current.value,
+        age: userAgeRef.current.value,
+        id: Math.random(),
+      },
     ]);
-    setUserName("");
-    setUserAge("");
   };
 
   const submitCheck = (event) => {
     event.preventDefault();
-    if (userName && userAge !== "" && userAge > 0) {
+    if (
+      userNameRef.current.value &&
+      userAgeRef.current.value !== "" &&
+      userAgeRef.current.value > 0
+    ) {
       handleSubmit();
     } else {
       setIsModalOpen(true);
@@ -39,23 +37,11 @@ const Form = (props) => {
       <FormContainer>
         <Label htmlForm="name">Name</Label>
         <br />
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          value={userName}
-          onChange={handleNameInput}
-        />
+        <Input type="text" id="name" name="name" ref={userNameRef} />
         <br />
         <Label htmlForm="age">Age</Label>
         <br />
-        <Input
-          type="number"
-          id="age"
-          name="age"
-          value={userAge}
-          onChange={handleAgeInput}
-        />
+        <Input type="number" id="age" name="age" ref={userAgeRef} />
         <br />
         <Button onClick={submitCheck}>Add User</Button>
       </FormContainer>
